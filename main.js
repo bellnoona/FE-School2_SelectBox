922e2f8ac96d31255496c528d8c08084f92db41b
Soohyeon Kim <tngusglaso@gmail.com>
2022-04-19 14:18:48 +0900

    화살표 이미지 변경 이벤트 추가

--- main.js
+++ main.js
@@ -7,8 +7,22 @@ selectBox.addEventListener('click', () => {
   if (ulBox.classList.contains('invisible')) {
     ulBox.classList.remove('invisible');
     ulBox.classList.add('visible');
+
+    // 화살표 이미지 (위 아래) 변경
+    selectBox.classList.add('on');
   } else {
     ulBox.classList.add('invisible');
     ulBox.classList.remove('visible');
+
+    // 화살표 이미지 (위 아래) 변경
+    selectBox.classList.remove('on');
   }
+});
+
+// list 클릭 시 button에 나타내기
+
+const liBtn = document.querySelector('.li-btn');
+liBtn.addEventListener('click', (e) => {
+  console.log(e.target.nodeName);
+  console.log(e.target.innerText);
 });
