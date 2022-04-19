805c6f1fba7592d4f6dab84e720749f70322a004
Soohyeon Kim <tngusglaso@gmail.com>
2022-04-19 15:52:59 +0900

    js 선택 버그수정

--- main.js
+++ main.js
@@ -27,8 +27,14 @@ ulBox.addEventListener('click', (e) => {
 
   liBox.forEach((li) => {
     // console.log(li.innerText);
+
+    // button영역을 넘어서 li부분을 클릭했을 경우 => li의 childNodes[1] (= button태그 정보)를 target에 담아준다.
+    const target =
+      e.target.nodeName === 'BUTTON' ? e.target : e.target.childNodes[1];
+
+    console.log(target);
     if (li.innerText) {
-      selectBox.innerText = e.target.innerText;
+      selectBox.innerText = target.innerText;
 
       // 글자 표시해준 후 박스 닫아주기
       ulBox.classList.add('invisible');
@@ -39,3 +45,5 @@ ulBox.addEventListener('click', (e) => {
 });
 
 // 콘솔문의 추후에 제거. 헷갈려서 겁나 찍어놓았음.
+// ** 수정사항 **
+// 1. <li>태그에서 button영역 밖을 클릭하면 UI가 깨지는 버그 해결.
